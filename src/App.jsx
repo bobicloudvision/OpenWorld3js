2e6366cdadf9f99b702067646b594202cb684d3e
Bozhidar <bobicloudvision@gmail.com>
2025-10-28 15:01:11 +0200

    up

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ export default function App() {
           <directionalLight intensity={2.5} castShadow shadow-bias={-0.0004} position={[-20, 20, 20]}>
             <orthographicCamera attach="shadow-camera" args={[-20, 20, 20, -20]} />
           </directionalLight>
-          <ambientLight intensity={0.2} />
+          <ambientLight intensity={2} />
 
           <Physics timeStep="vary">
           
